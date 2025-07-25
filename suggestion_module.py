f1fa2cc976baa5112af5a47abccf60219d2cbf94
Gayathri11-5 <gaya3veni11@gmail.com>
2025-07-25 11:39:47 +0530

    Update suggestion_module.py

--- suggestion_module.py
+++ suggestion_module.py
@@ -6,12 +6,15 @@ def marketing(profile_data):
         wines=row.get("MntWines",0)
         recency=row.get("Recency",0)
         teenhome=row.get("Teenhome",0)
+        web=row.get("NumWebPurchases",0)
         if income > 0.8 and wines >500
             sug = f"cluster{idx} 💎 Offer premium memberships and loyalty rewards"
         elif recency < 10:
             sug =  f"cluster{idx}⏰ Send time-sensitive limited-time deals"
         elif teenhome > 0:
             sug =  f"cluster{idx}🎮 Promote tech gadgets and teen-focused offers"
+        elif web >7:
+            sug=f"cluster{idx}focus their activity in purchasing and offer deals and discounts"
         else:
             sug="try discounts and offers for this season"
         suggestions.append(sug)
