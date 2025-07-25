bbabe7bd669d0f737db24b11f15021916121d9dc
Gayathri11-5 <gaya3veni11@gmail.com>
2025-07-25 11:29:14 +0530

    Update suggestion_module.py

--- suggestion_module.py
+++ suggestion_module.py
@@ -7,11 +7,11 @@ def marketing(profile_data):
         recency=row.get("Recency",0)
         teenhome=row.get("Teenhome",0)
         if income > 0.8 and wines >500
-            sug = f"Cluster{idx} 💎 Offer premium memberships and loyalty rewards"
+            sug = f"cluster{idx} 💎 Offer premium memberships and loyalty rewards"
         elif recency < 10:
-            sug =  f"Cluster{idx}⏰ Send time-sensitive limited-time deals"
+            sug =  f"cluster{idx}⏰ Send time-sensitive limited-time deals"
         elif teenhome > 0:
-            sug =  f"Cluster{idx}🎮 Promote tech gadgets and teen-focused offers"
+            sug =  f"cluster{idx}🎮 Promote tech gadgets and teen-focused offers"
         else:
             sug="try discounts and offers for this season"
         sugesstions.append(sug)
